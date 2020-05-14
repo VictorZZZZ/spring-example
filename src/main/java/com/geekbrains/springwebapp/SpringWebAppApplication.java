97292078d0a8bad78977d5882eeae1c54586d137
VictorZZZ <zvictormail@gmail.com>
2020-05-14 10:07:43 +0300

    initial commit

---
+++ src/main/java/com/geekbrains/springwebapp/SpringWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.geekbrains.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppApplication {
+	//todo добавить картинки к товарам
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppApplication.class, args);
+	}
+
+}
